94d19b3bbb5c97fe6ecc92028cb7099dc27266a9
luizhsalazar <luizhsalazar@gmail.com>
2015-11-21 18:39:51 -0200

    Adjusting tests controller

--- test/controllers/anagrams_controller_test.rb
+++ test/controllers/anagrams_controller_test.rb
@@ -1,49 +1,23 @@
 require 'test_helper'
 
 class AnagramsControllerTest < ActionController::TestCase
+
   setup do
-    @anagram = anagrams(:one)
+    @anagram = Anagram.new("sentence"=>"joão", "sentence2"=>"joao")
+    request.env["HTTP_REFERER"] = root_path
   end
 
   test "should get index" do
     get :index
     assert_response :success
-    assert_not_nil assigns(:anagrams)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
   end
 
   test "should create anagram" do
     assert_difference('Anagram.count') do
-      post :create, anagram: { is_anagram: @anagram.is_anagram, sentence: @anagram.sentence }
+      post :create, anagram: { is_anagram: @anagram.is_anagram, sentence: @anagram.sentence, sentence2: @anagram.sentence2 }
     end
 
-    assert_redirected_to anagram_path(assigns(:anagram))
+    assert_redirected_to root_path
   end
 
-  test "should show anagram" do
-    get :show, id: @anagram
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @anagram
-    assert_response :success
-  end
-
-  test "should update anagram" do
-    patch :update, id: @anagram, anagram: { is_anagram: @anagram.is_anagram, sentence: @anagram.sentence }
-    assert_redirected_to anagram_path(assigns(:anagram))
-  end
-
-  test "should destroy anagram" do
-    assert_difference('Anagram.count', -1) do
-      delete :destroy, id: @anagram
-    end
-
-    assert_redirected_to anagrams_path
-  end
 end
